70a7d580cca23829fd7b71cce81098ec00ccc27d
charles delepelaire <charles-web@hotmail.fr>
2024-03-09 21:25:16 +0100

    removed unused methods

--- MainConsole/Program.cs
+++ MainConsole/Program.cs
@@ -1,15 +1,14 @@
-﻿//using Sided.Example;
-//using Sided;
-//using System.Drawing;
-using Sided.WorkInProgress;
+﻿using Sided.Example;
+using Sided;
+using System.Drawing;
+//using Sided.WorkInProgress;
 namespace Main
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Field<string> list = new Field<string>("text");
-			Tree<string> arbre = list.Get();
+			ExampleUsage usage = new ExampleUsage();
 
 		}
 	}
--- SidedLib/Sided.cs
+++ SidedLib/Sided.cs
@@ -11,17 +11,26 @@ namespace Sided
 	/// <typeparam name="T"></typeparam>
 	public class Left<T>
 	{
-
 		private T Side { get; set; }
+
+		/// <summary>
+		/// left side setter
+		/// </summary>
+		/// <param name="value"></param>
 		public void Set(T value)
 		{
-				Side = value;	
+			Side = value;	
 		}
+		/// <summary>
+		/// left side getter
+		/// </summary>
+		/// <returns></returns>
 		public T Get()
 		{
 			return Side;
 		}
 	}
+
 	/// <summary>
 	/// Generic Class for Right side of Sided
 	/// </summary>
@@ -30,10 +39,18 @@ namespace Sided
 	{
 		private T Side { get; set; }
 
+		/// <summary>
+		/// Right side setter
+		/// </summary>
+		/// <param name="value"></param>
 		public void Set(T value)
 		{
 			Side = value;
 		}
+		/// <summary>
+		/// right side getter
+		/// </summary>
+		/// <returns></returns>
 		public T Get()
 		{
 			return Side;
@@ -49,45 +66,49 @@ namespace Sided
 		public Left<T> Left { get; private set; }
 		public Right<T> Right { get; private set; }
 
-
+		/// <summary>
+		/// empty constructor with both side init
+		/// </summary>
 		public Sided()
 		{
 			Left = new();
 			Right = new();
 		}
-		// left and right point to the same object, need to correct that
-		//public Sided(T both)
-		//{
-		//	this.Instanciate();
-		//	this.Left.Set(both);
-		//	this.Right.Set(both);
-		//}
+
+		/// <summary>
+		/// constructor with both side setting option
+		/// </summary>
+		/// <param name="left"></param>
+		/// <param name="right"></param>
 		public Sided(T left, T right)
 		{
 			Left = new();
 			Right = new();
-			Left.Set(left);
-			Right.Set(right);
-
+			SetBoth(left, right);
 		}
-		/// <summary>
-		/// left right point to same reference objet, nee to correct that
-		/// </summary>
-		//public void SetBoth(T both)
-		//{
-		//	this.Left.Set(both);
-		//	this.Right.Set(both);
-		//}
 
+		/// <summary>
+		/// set both side object
+		/// </summary>
+		/// <param name="left"></param>
+		/// <param name="right"></param>
 		public void SetBoth(T left, T right)
 		{
 			Left.Set(left);
 			Right.Set(right);
 		}
+		/// <summary>
+		/// set left side object
+		/// </summary>
+		/// <param name="left"></param>
 		public void SetSide(Left<T> left)
 		{
 			Left = left;
 		}
+		/// <summary>
+		/// set right side object
+		/// </summary>
+		/// <param name="left"></param>
 		public void SetSide(Right<T> right)
 		{
 			Right = right;
