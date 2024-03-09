53dd5a463413fa985c5f5f6e7f5027ace5825764
charles delepelaire <charles-web@hotmail.fr>
2024-03-09 21:19:48 +0100

    update : -added comment -slightly refactoring sided source code

--- MainConsole/Program.cs
+++ MainConsole/Program.cs
@@ -1,14 +1,15 @@
-﻿using Sided.Example;
-using Sided;
-using System.Drawing;
-
+﻿//using Sided.Example;
+//using Sided;
+//using System.Drawing;
+using Sided.WorkInProgress;
 namespace Main
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			ExampleUsage usage = new ExampleUsage();
+			Field<string> list = new Field<string>("text");
+			Tree<string> arbre = list.Get();
 
 		}
 	}
--- SidedLib/Sided.cs
+++ SidedLib/Sided.cs
@@ -15,11 +15,11 @@ namespace Sided
 		private T Side { get; set; }
 		public void Set(T value)
 		{
-				this.Side = value;	
+				Side = value;	
 		}
 		public T Get()
 		{
-			return this.Side;
+			return Side;
 		}
 	}
 	/// <summary>
@@ -32,11 +32,11 @@ namespace Sided
 
 		public void Set(T value)
 		{
-			this.Side = value;
+			Side = value;
 		}
 		public T Get()
 		{
-			return this.Side;
+			return Side;
 		}
 	}
 
@@ -46,12 +46,14 @@ namespace Sided
 	/// <typeparam name="T"></typeparam>
 	public class Sided<T>
 	{
-		public Left<T> Left;// { get; private set; }
-		public Right<T> Right;// { get; private set; }
+		public Left<T> Left { get; private set; }
+		public Right<T> Right { get; private set; }
+
 
 		public Sided()
 		{
-			this.Instanciate();
+			Left = new();
+			Right = new();
 		}
 		// left and right point to the same object, need to correct that
 		//public Sided(T both)
@@ -62,39 +64,33 @@ namespace Sided
 		//}
 		public Sided(T left, T right)
 		{
-			this.Instanciate();
-			this.Left.Set(left);
-			this.Right.Set(right);
+			Left = new();
+			Right = new();
+			Left.Set(left);
+			Right.Set(right);
 
 		}
-		private void Instanciate()
-		{
-			this.Left = new Left<T>();
-			this.Right = new Right<T>();
-		}
-
 		/// <summary>
 		/// left right point to same reference objet, nee to correct that
 		/// </summary>
 		//public void SetBoth(T both)
 		//{
-
 		//	this.Left.Set(both);
 		//	this.Right.Set(both);
 		//}
 
 		public void SetBoth(T left, T right)
 		{
-			this.Left.Set(left);
-			this.Right.Set(right);
+			Left.Set(left);
+			Right.Set(right);
 		}
 		public void SetSide(Left<T> left)
 		{
-			this.Left = left;
+			Left = left;
 		}
 		public void SetSide(Right<T> right)
 		{
-			this.Right = right;
+			Right = right;
 		}	
 	}
 }
